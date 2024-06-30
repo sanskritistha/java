689cb56d36bde6e113565bd20bd76b0b1edb92f5
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-06-30 18:58:02 +0545

    Array

---
+++ Array.java
@@ -0,0 +1,16 @@
+
+public class Array {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5,6,7,8,9,10,11};
+         int  s=0;
+         for(int i=0;i<=a[9];i++){
+             if(i%2==0){
+             s=s+a[i];
+         }
+         }
+         
+         System.out.println("sum="+s);
+        
+    }
+    
+}
