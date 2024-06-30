ccc720bd7f2c8ff7444acbb8805a107b99e93336
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-06-30 18:56:41 +0545

    Basic Program

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Add {
+    public static void main(String[] args) {
+  Scanner ob=new Scanner(System.in);
+  
+System.out.println("enter two numbers");
+int a= ob.nextInt();
+int b= ob.nextInt();
+int c=a+b;
+System.out.println("sum=" +c);
+
+    }
+    
+    
+}
