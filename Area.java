58711deaa6837701aabcbcdce6bbf582f6463ead
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-06-30 18:57:04 +0545

    Basic Program

---
+++ Area.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Area {
+    public static void main(String[] args) {
+        Scanner ob =new Scanner(System.in);
+System.out.println("enter the radius");
+double r=ob.nextDouble();
+double a=3.14*r*r;
+System.out.println("area="+a);
+}
+}
+    
+    
+    
+
