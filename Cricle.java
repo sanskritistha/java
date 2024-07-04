80b22c015a0ec279f95803a8118e89a90bce037a
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-07-04 18:16:08 +0545

    Basic Program

---
+++ Cricle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Cricle {
+
+    public static void main(String[] args) {
+  Scanner ob=new Scanner(System.in);
+  
+System.out.println("enter the radius of the cricle");
+int  r= ob.nextInt();
+ double area = 3.14*r*r;
+
+System.out.println("area of the circle is :" +area);
+
+    }
+    
+    
+}
+
+  
