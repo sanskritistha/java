bd6cd83b17c356320042bfeeb3abbe0ecbe1b1d3
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-07-04 18:15:00 +0545

    basicprogram

---
+++ CbrtRoot.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CbrtRoot{
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int a=ob.nextInt();
+        double r=Math.cbrt(a);
+        System.out.println(r+"cube=");
+        
+            
+    }
+}
