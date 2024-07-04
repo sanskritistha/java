32169d5f7fcf10b2edff12ccaceb9ae273aaa56c
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-07-04 18:15:40 +0545

    Decision making

---
+++ ConditionalOperators.java
@@ -0,0 +1,14 @@
+
+public class ConditionalOperators {
+    public static void main(String[] args) {
+        int a=10;
+        boolean x= a%2==0?true:false;
+        if(x){
+            System.out.println("Even");
+            
+        }else{
+            System.out.println("odd");
+        }
+        
+    }
+}
