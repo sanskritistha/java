2fb6329668fc2a7e849a26fd6aba21ac6680b6fb
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-07-15 19:28:07 +0545

    Array

---
+++ Foreach.java
@@ -0,0 +1,10 @@
+
+public class Foreach {
+    public static void main(String[] args) {
+        int a[]={5,6,7,8};
+        for(int m:a){
+            System.out.println(m);
+        }
+    }
+    
+}
