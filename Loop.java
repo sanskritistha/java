41730e60c66212c380d4c1d504c979ecc5c2bf1f
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-07-18 16:53:42 +0545

    array

---
+++ Loop.java
@@ -0,0 +1,11 @@
+
+public class Loop {
+    public static void main(String[] args) {
+        for( int i=101;i<200;i++){
+            if(i%5==0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
