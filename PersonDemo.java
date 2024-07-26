ec60ba778a5a627fffebedd315d53b64afe8b8cd
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-07-26 10:53:29 +0545

    OOP

---
+++ PersonDemo.java
@@ -0,0 +1,19 @@
+class Person{
+    String name,address;
+    int age;
+    
+}
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person p=new Person();
+        p.name="ram";
+        p.address="ktm";
+        p.age=25;
+        System.out.println("Name="+p.name);
+        System.out.println("Address="+p.address);
+        System.out.println("Age="+p.age);
+        
+        
+        
+    }
+}
