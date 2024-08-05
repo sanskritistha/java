68f0b62b8fd71f3e7280505a771845781e46a77b
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-08-05 17:03:36 +0545

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,12 +1,11 @@
 
-package generics;
-import java.util.*;
-
 public class Test {
     public static void main(String[] args) {
-        List<String> list=new ArrayList();
-       // list.add(10);
-        list.add("hello");
+        int i;
+        for(  i=1;i<=5;i++){
+            System.out.println(i);
+        }
+        
     }
     
 }
