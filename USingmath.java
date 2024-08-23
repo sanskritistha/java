3f7f7bee44056d1554dbed619bb3cda9a9bc32a2
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-08-23 19:20:41 +0545

    Add files via upload

---
+++ USingmath.java
@@ -0,0 +1,16 @@
+
+public class USingmath {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-50));
+        System.out.println(Math.sqrt(2));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.floor(2.5));
+        System.out.println(Math.ceil(2.5));
+        System.out.println(Math.cbrt(9));
+        System.out.println(Math.max(2,7));
+        System.out.println(Math.min(2,1));
+        
+        
+    }
+    
+}
